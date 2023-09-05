6dbb59d129c9d7f9eed48f6ffaa6c676d9fee9cd
Bimaselalubenar <bimaardiantopramana@gmail.com>
2023-09-05 19:58:19 +0700

    add source code

---
+++ tailwind.config.js
@@ -0,0 +1,78 @@
+module.exports = {
+  "theme": {
+    "extend": {
+      "colors": {
+        "Template": {
+          "blue-bg": "#5228f5ff"
+        },
+        "Primary": {
+          "100": "#003EB3",
+          "300": "#0074F0",
+          "500": "#14A9FF",
+          "700": "#85DCFF"
+        },
+        "Danger": {
+          "300": "#A22020",
+          "500": "#BF2626",
+          "700": "#E14747"
+        },
+        "Success": {
+          "300": "#199033",
+          "500": "#32A94C",
+          "700": "#4CC366"
+        },
+        "Gray": {
+          "500": "#595959",
+          "700": "#999999",
+          "900": "#D9D9D9",
+          "Black": "#000000",
+          "White": "#FFFFFF"
+        },
+        "secondary": {
+          "400": "#67748eff",
+          "700": "#20273fff"
+        },
+        "foreground": "#000000",
+        "background": "#FFFFFF"
+      },
+      "spacing": {
+        "ThreeUnits": "48px",
+        "OneAndHalfUnits": "24px",
+        "Unit": "16px",
+        "HalfUnit": "8px",
+        "FourUnits": "64px",
+        "unit6": "1rem",
+        "SevenUnits": "112px",
+        "unit4": "1rem",
+        "unit2": "1rem",
+        "TwoUnits": "32px",
+        "unit3": "1rem",
+        "SixUnits": "96px",
+        "FiveUnits": "80px"
+      },
+      "borderRadius": {
+        "radius75": "0.75rem",
+        "Radius2": "2px",
+        "Round": "50%",
+        "Radius4": "4px",
+        "Radius8": "8px",
+        "radius40": "40px",
+        "radius5": "0.5rem"
+      },
+      "scale": {
+        "XXLarge": "288px",
+        "XLarge": "192px",
+        "Small": "48px",
+        "MaxWidth": "1400px",
+        "Large": "144px",
+        "Medium": "96px",
+        "XSmall": "16px"
+      }
+    }
+  },
+  "plugins": [],
+  "content": [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./*.html"
+  ]
+}
