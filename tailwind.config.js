339c44a8b4bd2a0fbd9ef44d4c09b657c765e1f2
Bimaselalubenar <bimaardiantopramana@gmail.com>
2023-12-31 15:33:29 +0700

    asdasd

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,6 @@ module.exports = {
   "theme": {
     "extend": {
       "colors": {
-        "Template": {
-          "blue-bg": "#5228f5ff"
-        },
-        "Primary": {
-          "100": "#003EB3",
-          "300": "#0074F0",
-          "500": "#14A9FF",
-          "700": "#85DCFF"
-        },
-        "Danger": {
-          "300": "#A22020",
-          "500": "#BF2626",
-          "700": "#E14747"
-        },
         "Success": {
           "300": "#199033",
           "500": "#32A94C",
@@ -28,44 +14,43 @@ module.exports = {
           "Black": "#000000",
           "White": "#FFFFFF"
         },
-        "secondary": {
-          "400": "#67748eff",
-          "700": "#20273fff"
+        "Danger": {
+          "300": "#A22020",
+          "500": "#BF2626",
+          "700": "#E14747"
+        },
+        "Primary": {
+          "100": "#003EB3",
+          "300": "#0074F0",
+          "500": "#14A9FF",
+          "700": "#85DCFF"
         },
         "foreground": "#000000",
         "background": "#FFFFFF"
       },
       "spacing": {
+        "FiveUnits": "80px",
         "ThreeUnits": "48px",
-        "OneAndHalfUnits": "24px",
-        "Unit": "16px",
         "HalfUnit": "8px",
         "FourUnits": "64px",
-        "unit6": "1rem",
-        "SevenUnits": "112px",
-        "unit4": "1rem",
-        "unit2": "1rem",
-        "TwoUnits": "32px",
-        "unit3": "1rem",
         "SixUnits": "96px",
-        "FiveUnits": "80px"
+        "Unit": "16px",
+        "TwoUnits": "32px",
+        "OneAndHalfUnits": "24px"
       },
       "borderRadius": {
-        "radius75": "0.75rem",
         "Radius2": "2px",
-        "Round": "50%",
         "Radius4": "4px",
-        "Radius8": "8px",
-        "radius40": "40px",
-        "radius5": "0.5rem"
+        "Round": "50%",
+        "Radius8": "8px"
       },
       "scale": {
-        "XXLarge": "288px",
-        "XLarge": "192px",
-        "Small": "48px",
-        "MaxWidth": "1400px",
-        "Large": "144px",
         "Medium": "96px",
+        "XXLarge": "288px",
+        "Large": "144px",
+        "Small": "48px",
+        "XLarge": "192px",
+        "MaxWidth": "1400px",
         "XSmall": "16px"
       }
     }
